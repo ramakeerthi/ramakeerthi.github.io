3fe3b3bd783a97f568e3fc0b750d5fec3a67afb5
ramakeerthi <ramakeerthi09@gmail.com>
2026-01-18 15:54:45 +0530

    Updated deployment files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,15 +20,11 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://ramakeerthi.github.io',
+  baseUrl: '/dsa-notes/',
+  organizationName: 'ramakeerthi',
+  projectName: 'dsa-notes',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
